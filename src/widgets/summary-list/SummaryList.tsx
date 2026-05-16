93df392efa4d573b048da8eeaeff250528bc05eb
flipzky1337 <rorytheloli1337@gmail.com>
2026-05-16 13:05:42 +0500

    fix lintable

--- src/widgets/summary-list/SummaryList.tsx
+++ src/widgets/summary-list/SummaryList.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { useNavigate } from '@tanstack/react-router'
 import { useQueryClient } from '@tanstack/react-query'
 import { toast } from 'sonner'
 import type { Summary } from '@/entities/summary'
